2cb27b620d26896a92fb668fc7bdda1c1507c1c5
Dolev Artzi <dartzi@andrew.cmu.edu>
2022-10-25 14:41:01 -0400

    queue and collection

---
+++ queue.ts
@@ -0,0 +1,15 @@
+import { VariedOrderCollection } from './variedordercollection';
+export class Queue<T> extends VariedOrderCollection<T> {
+    add(elem: T) {
+        this.storage.push(elem);
+    }
+
+    remove(): T | undefined {
+        return this.storage.shift();
+    //     if (!x) {
+    //         throw new TypeError("Remove on empty queue");
+    //     }
+    //     return x;
+    // }
+    }
+}
---
+++ variedordercollection.ts
@@ -0,0 +1,14 @@
+export abstract class VariedOrderCollection<T> {
+    protected storage : T[];
+
+    constructor() {
+        this.storage = [];
+    }
+
+    size() : number {
+        return this.storage.length;
+    }
+
+    abstract add(elem : T);
+    abstract remove() : T | undefined;
+}
